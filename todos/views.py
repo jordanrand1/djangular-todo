0562cf15a861f9415e004cbb5c3b012a59875061
jorandrand1 <jorrand@gmail.com>
2018-11-14 16:41:50 -0700

    trying to fix post ;

---
+++ todos/views.py
@@ -0,0 +1,32 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Todo
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        print ({request})
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def todo_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        todo.delte()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
